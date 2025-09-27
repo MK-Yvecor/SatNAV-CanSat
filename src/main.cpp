5926c865efb70ed83373cb6bc9fbc889f7ff6643
MK-Yvecor <michal.korbiel.mk@gmail.com>
2025-09-27 23:14:24 +0200

    Memory test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
-
+#include <math.h>
 // Open serial port at baud rate 115200 to check if the coms are working properly
+
+float PSRAM_SIZE;
+float FLASH_SIZE;
 
 void setup() {
   // put your setup code here, to run once:
@@ -9,6 +12,21 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("TEST");
-  delay(500);
+  PSRAM_SIZE = ESP.getPsramSize();
+  Serial.print("PSRAM SIZE [KB]: ");
+  Serial.println(PSRAM_SIZE / 1024);
+  Serial.print("PSRAM SIZE [MB]: ");
+  Serial.println(PSRAM_SIZE / 1024 / 1024);
+
+  Serial.println("-=-=-=-=-=-=-=-=-=-=-=-=");
+
+  FLASH_SIZE = ESP.getFlashChipSize();
+  Serial.print("FLASH SIZE [KB]: ");
+  Serial.println(FLASH_SIZE / 1024);
+  Serial.print("FLASH SIZE [MB]: ");
+  Serial.println(FLASH_SIZE / 1024 / 1024);
+
+  Serial.println("-=-=-=-=-=-=-=-=-=-=-=-=");
+
+  delay(1000);
 }
