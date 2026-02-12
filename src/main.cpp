39099039f6a2607ebac7a8aab0de4cb005edc499
MK-Yvecor <michal.korbiel.mk@gmail.com>
2026-02-12 02:44:41 +0100

    minor_changes

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,19 @@
 #include <Adafruit_NeoPixel.h>
 #include "I2C_Sensors.h"
 #include <Adafruit_BMP280.h>
+#include <RadioLib.h>
+#include <SPI.h>
 
 #define LED_PIN 12
 #define LED_COUNT 1
+
+#define L_CS 33
+#define L_DIO0 37
+#define L_RST 38
+
+#define SCK 36
+#define MISO 34
+#define MOSI 35
 
 // I2C Pins
 // SDA = 18, SCL = 17 
@@ -15,7 +25,11 @@
 // EX_01 Address 0x40
 
 using namespace I2C_Sensors;
+
+SPIClass spiLoRa(FSPI);
+
 INA219 INA219_(INA219_ADDR, 0.05f, 2.0f);
+SX1278 radio = new Module(L_CS, L_DIO0, L_RST, -1, spiLoRa);
 Adafruit_BMP280 _BMP280;
 EX_01 EX_01_(0x40, 2, 5, 10);
 MPU6050 MPU;
@@ -34,45 +48,82 @@ MPU6050::GyroData GyroData;
 MPU6050::AccelData AccelData;
 
 void setup() {
-  // put your setup code here, to run once:
+  delay(500);
+
   Serial.begin(115200);
   Wire.begin(18,17);
-
+  
+  spiLoRa.begin(SCK, MISO, MOSI, L_CS);
 
   pinMode(2, INPUT_PULLUP);
   pinMode(5, INPUT_PULLUP);
 
-  if(!_BMP280.begin(0x76)){
+  WS2812B.begin();
+  
+  /*if(!_BMP280.begin(0x76)){
+    Serial.println("Failed to initialize BMP280");
+    WS2812B.setPixelColor(50,0,0,0);
+    WS2812B.show();
+    delay(3000);
     return;
   }
-
-    _BMP280.setSampling(
+  
+  _BMP280.setSampling(
     Adafruit_BMP280::MODE_NORMAL,
     Adafruit_BMP280::SAMPLING_X2,
     Adafruit_BMP280::SAMPLING_X16,
     Adafruit_BMP280::FILTER_X16,
     Adafruit_BMP280::STANDBY_MS_500
   );
-
+  
   if (!MPU.ConfigureSensor()) {   // 0x68 I2C Address
     Serial.println("MPU6050 not found!");
-    while (1);
+    WS2812B.setPixelColor(50,0,0,0);
+    WS2812B.show();
+    delay(3000);
+    return;
   }
-
+  
   if(!INA219_.Init()){
     Serial.println("Failed to initialize INA219");
+    WS2812B.setPixelColor(50,0,0,0);
+    WS2812B.show();
+    delay(3000);
     return;
   }
-
+  
   if(!INA219_.ConfigureSensor()){
-    Serial.println("Failed to configure INA219");
-    return;
+      WS2812B.setPixelColor(50,0,0,0);
+      WS2812B.show();
+      Serial.println("Failed to configure INA219");
+      delay(3000);
+      return;
   }
   EX_01_.ads_reset();
   EX_01_.ads_write_reg(0x01, 0x00);
+  */
+  
+  int state = radio.begin(433.0, 125.0, 9, 7, 0x12, 17);
 
-
-  WS2812B.begin();
+  if(state != RADIOLIB_ERR_NONE){
+    WS2812B.setPixelColor(50,0,0,0);
+    WS2812B.show();
+    Serial.print("Failed to initialize LoRa, ERR Code: ");
+    Serial.println(state);
+    delay(3000);
+    return;
+  }
+  else { 
+    Serial.println("Radio Ready"); 
+    for(int i = 0; i<=5; i++){
+      delay(250);
+      WS2812B.setPixelColor(0,50,50,0);
+      WS2812B.show();
+      delay(250);
+      WS2812B.setPixelColor(0,0,00,0);
+      WS2812B.show();
+    }
+  }
 }
 
 void loop() {
@@ -92,6 +143,18 @@ void loop() {
   presssure = _BMP280.readPressure() / 100;
   altitude = _BMP280.readAltitude();
 
+  int state = radio.transmit("HELLO FROM LORA");
+
+  if(state == RADIOLIB_ERR_NONE){
+    Serial.println("TX Success");
+  }
+  else{
+    Serial.print("Failed to TX, ERR CODE: ");
+    Serial.println(state);
+  }
+
+  delay(1000);
+  /*
 
   Serial.print("TE");
   Serial.println(temperature);
@@ -137,7 +200,7 @@ void loop() {
 
    Serial.print("AZ");
    Serial.println(AccelData.AZ);
-
+   */
   delay(250);
   WS2812B.setPixelColor(0,0,0,0);
   WS2812B.show();
